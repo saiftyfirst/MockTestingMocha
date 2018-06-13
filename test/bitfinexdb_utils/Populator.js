7ce6e725498251e862e4956596777bdcc1eb52d7
Charlie Zhang <blokaly@users.noreply.github.com>
2018-06-13 17:17:32 +0800

    added async and mongoose

--- test/bitfinexdb_utils/Populator.js
+++ test/bitfinexdb_utils/Populator.js
@@ -1,28 +1,44 @@
-var MongoClient = require('mongodb').MongoClient;
-var data = require('./data.json');
-let client;
+import 'babel-polyfill';
+import Mongoose from 'mongoose';
+import async from 'async';
 
-var collections = ['test_1', 'test_2', 'test_3', 'test_4' ,'test_5' ,'test_6'];
+const collections = ['test_1', 'test_2', 'test_3', 'test_4' ,'test_5' ,'test_6', 'test_7', 'test_8', 'test_9', 'test_10'];
+const schema = new Mongoose.Schema({ name: 'string', size: 'string' });
 
-function customInsertMany(db){
-    collections.forEach(element => {
-        db.collection(element).insert(data[element], {ordered: false});
-        //console.log("Pika");
-    });
-    //db.collection(collection).insert(data[collection]);
+function customInsertMany(done) {
+   async.eachSeries(collections, (element, callback) => {
+      let Res = Mongoose.model(element, schema);
+      let small = new Res({name: element, size: 'small'});
+      small.save(function(err){
+         if(err) {
+            callback(err)
+         }
+         else {
+            console.log("inserted", element);
+            callback()
+         }
+      });
+   }, (err) => {
+      if(err) {
+        throw err;
+      }
+      else {
+        done();
+      }
+   });
 }
 
 function customDropMany(db){
-    collections.forEach(element => {
-        db.dropCollection(element);
-    });
+   collections.forEach(element => {
+      db.dropCollection(element);
+   });
 }
 
 module.exports = {
-    populate: function(db) {  
-        customInsertMany(db);    
-    },
-    unPopulate: function(db){
-        customDropMany(db);
-    }
+   populate: function(done) {
+      customInsertMany(done);
+   },
+   unPopulate: function(db){
+      customDropMany(db);
+   }
 };
--- test/test_bitfinex.js
+++ test/test_bitfinex.js
@@ -1,81 +1,102 @@
-var express = require('express'),
-    supertest = require('supertest'),
-    api = supertest('localhost:3000'),
-    expect = require('chai').expect;
+// var express = require('express'),
+//     supertest = require('supertest'),
+//     api = supertest('localhost:3000'),
+//     expect = require('chai').expect;
+import 'babel-polyfill';
+import Mongoose from 'mongoose';
+import chai from 'chai';
+import Populator from './bitfinexdb_utils/Populator';
+const Schema = Mongoose.Schema;
+const expect = chai.expect;
 
+const options = {
+   autoIndex: false, // Don't build indexes
+   reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+   reconnectInterval: 500, // Reconnect every 500ms
+   poolSize: 10, // Maintain up to 10 socket connections
+   // If not connected, return errors immediately rather than waiting for reconnect
+   bufferMaxEntries: 0
+};
 
-describe('Database Tests', function(){
+describe('Database Tests', () => {
+
     before(function(done){
-        api.get('/v1/addData')
-            .end(function (err, res) {
-                done();
-            });
-    })
-        
-    it('Should not be null', function(done){
-        api.get('/v1/symbols')
-            .set('test_id', '1')
-            //.expect(400)
-            .end(function (err, res) {
-                console.log(res.body);
-                done();
-            });
-    });
-    
-    it('Should not be null', function(done){
-        api.get('/v1/symbols')
-            .set('test_id', '2')
-            //.expect(400)
-            .end(function (err, res) {
-                console.log(res.body);
-                done();
-            });
-    });
-    
-    it('Should not be null', function(done){
-        api.get('/v1/symbols')
-            .set('test_id', '3')
-            //.expect(400)
-            .end(function (err, res) {
-                console.log(res.body);
-                done();
-            });
+       Mongoose.connect('mongodb://localhost/testDatabase', options);
+       let db = Mongoose.connection;
+       db.on('error', console.error.bind(console, 'connection error'));
+       db.once('open', function() {
+          console.log('We are connected to test database!');
+          Populator.populate(done);
+       });
     });
 
-    it('Should not be null', function(done){
-        api.get('/v1/symbols')
-            .set('test_id', '4')
-            //.expect(400)
-            .end(function (err, res) {
-                console.log(res.body);
-                done();
-            });
-    });
+   it("test1", function () {
+      console.log("top test1");
+   });
 
-    it('Should not be null', function(done){
-        api.get('/v1/symbols')
-            .set('test_id', '5')
-            //.expect(400)
-            .end(function (err, res) {
-                console.log(res.body);
-                done();
-            });
-    });
+    // it('Should not be null', (done) => {
+        // api.get('/v1/symbols')
+        //     .set('test_id', '1')
+        //     //.expect(400)
+        //     .end(function (err, res) {
+        //         console.log(res.body);
+        //         done();
+        //     });
+    // });
 
-    it('Should not be null', function(done){
-        api.get('/v1/symbols')
-            .set('test_id', '6')
-            //.expect(400)
-            .end(function (err, res) {
-                console.log(res.body);
-                done();
-            });
-    });
+    // it('Should not be null', function(done){
+    //     api.get('/v1/symbols')
+    //         .set('test_id', '2')
+    //         //.expect(400)
+    //         .end(function (err, res) {
+    //             console.log(res.body);
+    //             done();
+    //         });
+    // });
+    //
+    // it('Should not be null', function(done){
+    //     api.get('/v1/symbols')
+    //         .set('test_id', '3')
+    //         //.expect(400)
+    //         .end(function (err, res) {
+    //             console.log(res.body);
+    //             done();
+    //         });
+    // });
+    //
+    // it('Should not be null', function(done){
+    //     api.get('/v1/symbols')
+    //         .set('test_id', '4')
+    //         //.expect(400)
+    //         .end(function (err, res) {
+    //             console.log(res.body);
+    //             done();
+    //         });
+    // });
+    //
+    // it('Should not be null', function(done){
+    //     api.get('/v1/symbols')
+    //         .set('test_id', '5')
+    //         //.expect(400)
+    //         .end(function (err, res) {
+    //             console.log(res.body);
+    //             done();
+    //         });
+    // });
+    //
+    // it('Should not be null', function(done){
+    //     api.get('/v1/symbols')
+    //         .set('test_id', '6')
+    //         //.expect(400)
+    //         .end(function (err, res) {
+    //             console.log(res.body);
+    //             done();
+    //         });
+    // });
 
-    after(function(done){
-        api.get('/v1/removeData')
-            .end(function (err, res) {
-                done();
-            });
-    })
+    // after(function(done){
+    //    Mongoose.connection.db.dropDatabase(function(){
+    //       Mongoose.connection.close(done);
+    //    });
+    // })
 });
