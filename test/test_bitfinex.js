42bfd401ad9197e860f06fea707a54504db3facc
saifty <ahmedsaif.developer@gmail.com>
2018-06-15 03:47:51 +0600

    Added mongoose insert and drop utilities

--- test/test_bitfinex.js
+++ test/test_bitfinex.js
@@ -8,6 +8,8 @@ import chai from 'chai';
 import Populator from './bitfinexdb_utils/Populator';
 const Schema = Mongoose.Schema;
 const expect = chai.expect;
+const dbName = 'response_db';
+const data = require('./bitfinexdb_utils/data.json');
 
 const options = {
    autoIndex: false, // Don't build indexes
@@ -18,22 +20,53 @@ const options = {
    bufferMaxEntries: 0
 };
 
+let testCount = 1 ;
+let testSchema = new Mongoose.Schema({ response : Object});
+
 describe('Database Tests', () => {
 
     before(function(done){
-       Mongoose.connect('mongodb://localhost/testDatabase', options);
-       let db = Mongoose.connection;
-       db.on('error', console.error.bind(console, 'connection error'));
-       db.once('open', function() {
-          console.log('We are connected to test database!');
-          Populator.populate(done);
-       });
+        Mongoose.connect('mongodb://localhost/' + dbName, options);
+        Mongoose.connection.once('open', () => {
+            console.log("Connected to db");
+            done();
+        }).on('error', () => console.log("Error"));
+    })
+
+    beforeEach(function(done){
+        let model = Mongoose.model('test_' + testCount, testSchema);
+        let newObj = new model({ response : data['test_' + testCount++]});
+        newObj.save((err) => {
+            if(err) console.log(err);
+            done();
+        });
+    })
+
+    it("test1", function () {
+      console.log("top test1");
+
+    });
+    
+    it("test2", function () {
+        console.log("top test1");
+    });
+    
+    it("test3", function () {
+        console.log("top test1");
+    });
+    
+    it("test4", function () {
+        console.log("top test1");
+    });
+    
+    it("test5", function () {
+        console.log("top test1");
     });
 
-   it("test1", function () {
-      console.log("top test1");
-   });
-
+    it("test6", function () {
+        console.log("top test1");
+    });
+    
     // it('Should not be null', (done) => {
         // api.get('/v1/symbols')
         //     .set('test_id', '1')
@@ -95,8 +128,8 @@ describe('Database Tests', () => {
     // });
 
     // after(function(done){
-    //    Mongoose.connection.db.dropDatabase(function(){
-    //       Mongoose.connection.close(done);
-    //    });
+    //     Mongoose.connection.db.dropDatabase(function(){
+    //        Mongoose.connection.close(done);
+    //     });
     // })
 });
